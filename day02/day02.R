b72a2c7c106d6335df33c37f336b7a1094d3014c
Colin Rundel <rundel@gmail.com>
2022-12-02 23:38:43 -0500

    Day 2

--- day02/day02.R
+++ day02/day02.R
@@ -18,8 +18,40 @@ res = c("A X" = 3,
   "C Z" = 3)
 
 
-input
+d = tibble(
+  game = input
+) |>
+  mutate(
+    x = str_split(game," "),
+    play = map_chr(x, 1),
+    res = map_chr(x,2)
+  )
+
+sum(res[d$game])+sum(scores[d$res])
 
 ## Task 2
 
-input
+d2 = tibble(
+  game = input
+) |>
+  mutate(
+    x = str_split(game," "),
+    play = map_chr(x, 1),
+    out = map_chr(x, 2),
+    res = case_when(
+      out == "X" & play == "A" ~ "C",
+      out == "X" & play == "B" ~ "A",
+      out == "X" & play == "C" ~ "B",
+      out == "Y" & play == "A" ~ "A",
+      out == "Y" & play == "B" ~ "B",
+      out == "Y" & play == "C" ~ "C",
+      out == "Z" & play == "A" ~ "B",
+      out == "Z" & play == "B" ~ "C",
+      out == "Z" & play == "C" ~ "A"
+    )
+  )
+
+scores = c(A=1,B=2,C=3)
+out_score = c(X=0,Y=3,Z=6)
+
+sum(scores[d2$res])+sum(out_score[d2$out])
