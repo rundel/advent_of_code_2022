bd0558ce404110defa0c68186e220038b77cc6be
Colin Rundel <rundel@gmail.com>
2022-12-04 00:16:49 -0500

    now with dplyr

--- day04/day04.R
+++ day04/day04.R
@@ -42,3 +42,24 @@ input |>
   ) |>
   unlist() |>
   sum()
+
+
+## Task 1&2 (dplyr)
+
+tibble(input = input) |>
+  separate(input, ",",into = c("x","y")) |>
+  mutate(id = 1:n()) |>
+  pivot_longer(x:y) |>
+  separate(value, "-", into=c("start","stop")) |>
+  mutate(values = map2(start, stop, seq)) |>
+  select(id, name, values) |>
+  pivot_wider(id, names_from =name, values_from = values) |>
+  mutate(
+    task1 = map2_lgl(x,y,~ all(.x %in% .y) | all(.y %in% .x)),
+    task2 = map2_lgl(x,y,~ length(intersect(.x,.y)) > 0)
+  ) |>
+  summarize(
+    sum(task1),
+    sum(task2)
+  )
+
