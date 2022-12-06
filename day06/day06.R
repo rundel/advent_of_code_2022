beeacd6e3ca2dcc486545d69395f7a3803efabc1
Colin Rundel <rundel@gmail.com>
2022-12-06 00:55:42 -0500

    better

--- day06/day06.R
+++ day06/day06.R
@@ -50,3 +50,41 @@ for(i in seq_along(res[[1]])) {
     break 
   }
 }
+
+
+
+## Task 1 - take 2
+
+make_lags = function(x,n) {
+  l = lapply(seq_len(n), function(i) lag(x, i-1))
+  map(seq_along(l[[1]]), ~ map_chr(l, .x))
+}
+
+tibble(input = input) |>
+  mutate(
+    letters = str_split(input, "")
+  ) %>%
+  select(letters) %>%
+  unnest_longer(letters) %>%
+  mutate(
+    id = 1:n(),
+    lags = letters %>% make_lags(4),
+    status = map_lgl(lags, ~ length(unique(.x)) == 4 & all(!is.na(.x))) 
+  ) %>%
+  filter(status)
+
+## Task 2
+
+tibble(input = input) |>
+  mutate(
+    letters = str_split(input, "")
+  ) %>%
+  select(letters) %>%
+  unnest_longer(letters) %>%
+  mutate(
+    id = 1:n(),
+    lags = letters %>% make_lags(14),
+    status = map_lgl(lags, ~ length(unique(.x)) == 14 & all(!is.na(.x))) 
+  ) %>%
+  filter(status)
+  
